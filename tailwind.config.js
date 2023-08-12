fec22e5a952c9c177a8b603cb018cbaa7a177ca5
Kevin Renskers <kevin@loopwerk.io>
2023-08-12 13:19:10 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        link: "#ffffff",
+        loopwerk: "#f1a948",
+        linkedin: "#0a66c2",
+      },
+    },
+  },
+  plugins: [],
+};
